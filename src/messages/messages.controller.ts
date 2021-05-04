8260a45eaf81129ab0934d993b5f8651dfed3b95
Jonas <jbrionestecnico@gmail.com>
2021-05-04 16:47:20 -0400

    modificaciones en el script

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message-dto';
+
 
 @Controller('messages')
 export class MessagesController {
 
  @Post()
- create(@Body createMessageDto: CreateMensajeDto ){
-     
+ create(@Body createMessageDto: CreateMessageDto ){
+
  }   
 }
